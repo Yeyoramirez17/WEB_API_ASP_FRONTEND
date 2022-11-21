78dab1f2b8576b6655f49ff74e45de7d5bb3a53b
Yeison Ramirez <yeisonrazar98@gmail.com>
2022-11-21 17:07:24 -0500

    CRUD de estudiantes Completo

--- Interface/InterfaceStudent.cs
+++ Interface/InterfaceStudent.cs
@@ -5,7 +5,11 @@ namespace WEB_API_FRONTEND.Interface
     public interface InterfaceStudent
     {
         Task<List<Student>> GetAll();
+        Task<Student> GetStudentById(int IdStudent);
         Task<StudentCourse> GetStudentAndCoursesById(int idStudent);
+        Task<Student> CreateStudent(Student student);
+        Task UpdateStudent(int idStudent, Student student);
+        Task DeleteStudent(int idStudent);
     }
 }
 
--- Services/StudentService.cs
+++ Services/StudentService.cs
@@ -14,9 +14,22 @@ namespace WEB_API_FRONTEND.Services
         {
             return await httpClient.GetFromJsonAsync<List<Student>>("api/Students/GetAll");
         }
-
+        public async Task<Student> GetStudentById(int idStudent) {
+            return await httpClient.GetFromJsonAsync<Student>($"api/Students/StudentById/{idStudent}");
+        }
         public async Task<StudentCourse> GetStudentAndCoursesById(int idStudent){
             return await httpClient.GetFromJsonAsync<StudentCourse>($"api/Students/StudentAndCourses/{idStudent}");
+        }
+        public async Task<Student> CreateStudent(Student student) {
+            var result = await httpClient.PostAsJsonAsync<Student>("api/Students/CreateStudent", student);
+            var newStudent = await result.Content.ReadFromJsonAsync<Student>();
+            return newStudent;
+        }
+        public async Task UpdateStudent(int idStudent, Student student){
+            await httpClient.PutAsJsonAsync<Student>($"api/Students/UpdateStudent/{idStudent}", student);
+        }
+        public async Task DeleteStudent(int idStudent){
+            await httpClient.DeleteAsync($"api/Students/DeleteStudent/{idStudent}");
         }
     }
 }
